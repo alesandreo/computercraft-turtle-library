309f910363428f313a558111a3fe9c9eebc6f22f
amcconaughey <amcconaughey@gmail.com>
2021-01-20 09:05:29 -0800

    Add the initial release for working with the blood altar.

---
+++ autocrafter.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: amcco
+-- Date: 1/18/2021
+-- Time: 2:35 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+function get_slot()
+
+end
