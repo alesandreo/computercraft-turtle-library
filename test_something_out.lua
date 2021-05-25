5fb388a3473b9073a631e98be72b60530a979985
Alexander McConaughey <alex@yellowbrick.com>
2021-05-25 14:30:41 -0700

    Changes on my work comp.

--- test_something_out.lua
+++ test_something_out.lua
@@ -5,6 +5,18 @@
 -- Time: 11:03 PM
 -- To change this template use File | Settings | File Templates.
 --
-dofile("/t.lua")
 
 
+count=0
+count_2=0
+repeat
+    print(count)
+    count = count + 1
+    count_2 = count_2 + 1
+until (count >= 10 or count_2 >= 5)
+
+count = 0
+for i=1,16 do
+    count = count + 1
+    print(count)
+end
