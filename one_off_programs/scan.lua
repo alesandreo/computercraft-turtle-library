2c79495b77489ce5afccbb6285eddd55f5bb610c
amcconaughey <amcconaughey@gmail.com>
2022-03-30 17:29:51 -0700

    Updated my example scripts with comments.

--- one_off_programs/scan.lua
+++ one_off_programs/scan.lua
@@ -1,43 +1,55 @@
+-- 
+
+-- Example Usage: scan netherite
+-- will scan for netherite within the maximum freeRadius
+
+-- Stores arguments passed from the command line into a table.
 args = {...}
 scanner = peripheral.wrap("back")
+-- the first argument is [1]
 target_block = args[1]
---target_block = "minecraft:netherrack"
 if not scanner then
     error("Requires Scanner")
 end
 
-function compare_distance(a, b)
-    return a.distance < b.distance
-end
+-- function compare_distance(a, b)
+--     return a.distance < b.distance
+-- end
+
+-- function that searches all returned blocks for the substring passed.
 function search(tbl, k2)
     if tbl then
         local results = {}
         for k, v in ipairs(tbl) do
-        if string.find(v.name, k2) then
-            term.setBackgroundColor(colors.green)
-            v.distance = math.floor(math.sqrt(math.pow(math.abs(v.x), 2) + math.pow(math.abs(v.y), 2) + math.pow(math.abs(v.z), 2)))
-            table.insert(results, v)
-            --print(v.name.."\n"..distance..":"..v.x..','..v.y..','..v.z)
-        end
+            if string.find(v.name, k2) then
+                -- Makes the terminal turn green if you find anything.
+                term.setBackgroundColor(colors.green)
+                v.distance = math.floor(math.sqrt(math.pow(math.abs(v.x), 2) + math.pow(math.abs(v.y), 2) + math.pow(math.abs(v.z), 2)))
+                table.insert(results, v)
+            end
         end
         if #results == 0 then
             term.setBackgroundColor(colors.red)
-        else 
+        else
+            -- Eventually, I'll make the tables sort by distance
             for k, v in ipairs(results) do
                 print(v.distance.." "..v.name.."\n".."("..v.x..","..v.y..","..v.z..")")
+            end
         end
-        end
-       end
+    end
 end
 count = 0
 term.setBackgroundColor(colors.red)
 while true do
-term.setCursorPos(1,1)
-term.clear()
-count = count + 1
-print("Scan #"..count)
-search(scanner.scan(8), target_block)
-while scanner.getOperationCooldown("scanBlocks") > 0 do
-os.sleep(0.1)
-end
+    term.setCursorPos(1,1)
+    term.clear()
+    count = count + 1
+    print("Scan #"..count)
+    -- scanner.scan(radius) returns all blocks in the given radius, and their relative coordinates.
+    -- The geoscanner has a maximum range for free scans which can be read from scanner.getConfiguration()
+    search(scanner.scan(scanner.getConfiguration().scanBlocks.maxFreeRadius), target_block)
+    -- scanBlocks by default has a cooldown, this has the program wait until it has cooled for usage.
+    while scanner.getOperationCooldown("scanBlocks") > 0 do
+        os.sleep(0.1)
+    end
 end
--- one_off_programs/search_chunk.lua
+++ one_off_programs/search_chunk.lua
@@ -1,10 +1,14 @@
+-- f4MZ8ATi
+-- Stores arguments passed from the command line into a table.
 args = {...}
 scanner = peripheral.wrap("back")
+-- the first argument is [1]
 target_block = args[1]
 if not scanner then
     error("Requires Scanner")
 end
- 
+
+-- Function that just searches through a table's keys for a substring.
 function search(tbl, k2)
     if tbl then
         for k, v in pairs(tbl) do
@@ -14,15 +18,18 @@ function search(tbl, k2)
             end
         end
     end
-end 
+end
+
+-- This while loop just continues while you walk around.
 count = 0
 while true do
-count = count + 1
-term.setCursorPos(1,1)
-term.clear()
-print("Scan #"..count)
-search(scanner.chunkAnalyze(), target_block)
-while ( scanner.getOperationCooldown("scanBlocks") > 0 ) do
-os.sleep(0.1)
-end
+    count = count + 1
+    term.setCursorPos(1,1)
+    term.clear()
+    print("Scan #"..count)
+    -- scanner.chunkAnalyze returns a list of all ORE in the chunk, and its quantity.
+    search(scanner.chunkAnalyze(), target_block)
+    while ( scanner.getOperationCooldown("scanBlocks") > 0 ) do
+        os.sleep(0.1)
+    end
 end
